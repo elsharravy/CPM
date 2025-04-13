7d03c1e3e649d204df9d293ac6e785e7a0df9460
MarekKubi <147140266+MarekKubi@users.noreply.github.com>
2025-04-13 23:15:41 +0200

    Add files via upload
    Compiler z testami ręcznymi

---
+++ Compiler_class.py
@@ -0,0 +1,64 @@
+from re import A
+import node_class
+
+
+class Compiler(object):
+    def __init__(self):
+        a=0
+    @staticmethod
+    def Compile(nodes):
+        leng = len(nodes)
+        
+        ne_tab = list(range(leng))
+        for i in range(leng):
+            ne_tab[i] = 0
+        for i in range(leng):
+            for x in (nodes[i]).previous:
+                ne_tab[x] +=1
+        for i in range(leng): 
+            (nodes[i]).next =list()
+        
+        
+
+        for i in range(leng):
+            for x in (nodes[i]).previous:
+                nodes[x].next.append(i)
+        """   
+        for i in range(leng): 
+            print("node " + str(i) + ":")
+            #(nodes[i]).next =list(range(ne_tab[i]))
+            for j in range(len(nodes[i].next)):
+                print(" " + str(nodes[i].next[j]))
+                #(nodes[i]).next.append(ne_tab[j])
+        for i in range(leng): 
+            print("node " + str(i) + ":")
+            #(nodes[i]).next =list(range(ne_tab[i]))
+            for j in range(len(nodes[i].previous)):
+                print(" " + str(nodes[i].previous[j]))
+                #(nodes[i]).next.append(ne_tab[j])
+        """
+        for i in range(leng):
+            max_e = 0
+            for x in (nodes[i]).previous:
+                if(max_e<nodes[x].early_finish):
+                    max_e = nodes[x].early_finish
+            (nodes[i]).early_start = max_e
+            (nodes[i]).early_finish = (nodes[i]).early_start + (nodes[i]).duration
+        
+        for i in range(leng):
+            k =leng - i -1
+            if(len(nodes[k].next) == 0):
+                min_l = nodes[k].early_finish
+            else:
+                min_l = nodes[nodes[k].next[0]].late_start
+                for x in (nodes[k]).next:
+                    if(min_l>nodes[x].late_start):
+                        min_l = nodes[x].late_start
+            (nodes[k]).late_finish = min_l
+            (nodes[k]).late_start = (nodes[k]).late_finish - (nodes[k]).duration
+            (nodes[k]).reserve = (nodes[k]).late_start - (nodes[k]).early_start
+        return nodes
+            
+
+
+
--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,8 +1,10 @@
 import node_class
-from json_reader import Json_reader
+import Compiler_class
 
-tab1 = [1,2]
-tab2 = [3,4]
+tab1 = []
+tab2 = [0]
+tab3 = [0]
+tab4 = [1,2]
 
 n = node_class.node(2, "test", tab1 ,0, 2, 2, 2, 4, tab2)
 
@@ -22,11 +24,17 @@ n2.set_es(2)
       
 print("test2: " + str(n2.get_es()))
 
-#JSON Reader tests
 
-print("======= JSON READER TESTS==========")
+n1 = node_class.node(1, "a", tab1 )
+n2 = node_class.node(2, "b", tab2 )
+n3 = node_class.node(5, "c", tab3 )
+n4 = node_class.node(3, "d", tab4 )
 
-nodes = Json_reader.load("data/test_data/example.json")
+nodes = [n1,n2,n3,n4]
 
-for node in nodes.values():
-    print(f"{node.name}: Previous -> {[p.name for p in node.previous]}, Next -> {[n.name for n in node.next]}")
+nodes = Compiler_class.Compiler.Compile(nodes)
+#c(nodes)
+
+print("c : " + str(nodes[2].late_start))
+for x in nodes:
+    print("nazwa: " + str(x.get_n()) + "    es: "+ str(x.get_es()) + "    ls: "+ str(x.get_ls())+ "    ef: "+ str(x.get_ef()) + "    lf: "+ str(x.get_lf()) + "    rezerwa: " + str(x.get_r()) + "\n")
